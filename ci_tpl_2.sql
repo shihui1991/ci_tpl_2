f5feeb562fd60ba5560b5281bd529c2bd1de0c85
shihui1991 <452853270@qq.com>
2018-09-16 22:00:05 +0800

    更新sql

--- ci_tpl_2.sql
+++ ci_tpl_2.sql
@@ -19,7 +19,7 @@
 -- Current Database: `ci_tpl`
 --
 
-CREATE DATABASE IF NOT EXISTS `ci_tpl` DEFAULT CHARACTER SET utf8 ;
+CREATE DATABASE IF NOT EXISTS `ci_tpl` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `ci_tpl`;
 
@@ -94,6 +94,35 @@ LOCK TABLES `Cate` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `Config`
+--
+
+DROP TABLE IF EXISTS `Config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8 ;
+CREATE TABLE `Config` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Table` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '表名',
+  `Name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '名称',
+  `Columns` text CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '字段详情',
+  `Infos` varchar(255) DEFAULT NULL COMMENT '说明',
+  `Created` datetime DEFAULT NULL,
+  `Updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE,
+  KEY `Table` (`Table`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='快速配置';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Config`
+--
+
+LOCK TABLES `Config` WRITE;
+/*!40000 ALTER TABLE `Config` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Master`
 --
 
@@ -124,7 +153,7 @@ CREATE TABLE `Master` (
 
 LOCK TABLES `Master` WRITE;
 /*!40000 ALTER TABLE `Master` DISABLE KEYS */;
-INSERT INTO `Master` VALUES (1,'开v',1,'dev','$2y$10$n/kYlaZU7uleY.p0zWHaZ.8ipV68Yoy0vkLtL2sEm.UG3NzD/0j1S','6712FEDB-2A39-1273-473D-5F3347ADA0D3',1,'2018-09-09 18:42:11','2018-09-10 23:38:52');
+INSERT INTO `Master` VALUES (1,'aa',1,'dev','$2y$10$jJB1KYL7SCqo6ZltT0Tmpe0GbuYP.w/ewj3BISrGvppKU9ofK7Dhq','35828E0C-3FDD-A73E-DC6E-521F730B0505',1,'2018-09-09 18:42:11','2018-09-16 20:46:30');
 /*!40000 ALTER TABLE `Master` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -153,7 +182,7 @@ CREATE TABLE `Menu` (
   KEY `ParentId` (`ParentId`) USING BTREE,
   KEY `Url` (`Url`) USING BTREE,
   KEY `UrlAlias` (`UrlAlias`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台-菜单';
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台-菜单';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -162,7 +191,7 @@ CREATE TABLE `Menu` (
 
 LOCK TABLES `Menu` WRITE;
 /*!40000 ALTER TABLE `Menu` DISABLE KEYS */;
-INSERT INTO `Menu` VALUES (1,0,'/admin/menu#','','后台管理','<i class=\"layui-icon layui-icon-set-fill\"></i>',1,1,1,0,'','2018-09-08 23:45:50','2018-09-09 00:18:33'),(2,1,'/admin/menu','','菜单管理','<i class=\"layui-icon layui-icon-find-fill\"></i>',1,1,1,0,'','2018-09-08 23:51:02','2018-09-09 00:15:51'),(3,2,'/admin/menu/add','','添加菜单','',1,0,1,0,'','2018-09-09 10:21:40','0000-00-00 00:00:00'),(4,2,'/admin/menuedit','','修改菜单','',1,0,1,0,'','2018-09-09 10:22:07','0000-00-00 00:00:00'),(5,2,'/admin/menu/info','','菜单详情','',1,0,1,0,'','2018-09-09 10:23:05','0000-00-00 00:00:00'),(6,2,'/admin/menu/all','','所有菜单','',1,0,1,0,'','2018-09-09 10:23:33','0000-00-00 00:00:00'),(7,1,'/admin/role','','角色管理','<i class=\"layui-icon layui-icon-group\"></i>',1,1,1,0,'','2018-09-09 10:24:34','0000-00-00 00:00:00'),(8,7,'/admin/role/add','','添加角色','',1,0,1,0,'','2018-09-09 11:02:19','0000-00-00 00:00:00'),(9,7,'/admin/role/edit','','修改角色','',1,0,1,0,'','2018-09-09 11:04:55','0000-00-00 00:00:00'),(10,7,'/admin/role/info','','角色详情','',1,0,1,0,'','2018-09-09 11:14:33','0000-00-00 00:00:00'),(11,7,'/admin/role/all','','所有角色','',1,0,1,0,'','2018-09-09 11:14:58','0000-00-00 00:00:00'),(12,1,'/admin/master','','用户管理','<i class=\"layui-icon layui-icon-friends\"></i>',1,1,1,0,'','2018-09-09 17:16:04','0000-00-00 00:00:00'),(13,12,'/admin/master/add','','添加用户','',1,0,1,0,'','2018-09-09 17:16:35','2018-09-09 17:17:22'),(14,12,'/admin/master/edit','','修改用户','',1,0,1,0,'','2018-09-09 17:17:11','0000-00-00 00:00:00'),(15,0,'/admin/home#','','公共','<i class=\"layui-icon layui-icon-tips\"></i>',0,0,1,0,'','2018-09-09 23:48:57','0000-00-00 00:00:00'),(16,15,'/admin/home','','控制台','<i class=\"layui-icon layui-icon-console\"></i>',0,0,1,0,'','2018-09-09 23:49:32','0000-00-00 00:00:00'),(17,15,'/admin/home/nav','','导航菜单','',0,0,1,0,'','2018-09-09 23:50:36','0000-00-00 00:00:00'),(18,15,'/admin/home/upload','','文件上传','',0,0,1,0,'','2018-09-09 23:51:17','0000-00-00 00:00:00'),(19,0,'/admin/api#','','系统资料','<i class=\"layui-icon layui-icon-tabs\"></i>',1,1,1,0,'','2018-09-09 23:59:00','0000-00-00 00:00:00'),(20,19,'/admin/api','','接口文档','<i class=\"layui-icon layui-icon-link\"></i>',1,1,1,0,'','2018-09-10 00:04:29','0000-00-00 00:00:00'),(21,20,'/admin/api/add','','添加接口','',1,0,1,0,'','2018-09-10 00:05:02','0000-00-00 00:00:00'),(22,20,'/admin/api/edit','','修改接口','',1,0,1,0,'','2018-09-10 00:05:27','0000-00-00 00:00:00'),(23,20,'/admin/api/info','','接口信息','',1,0,1,0,'','2018-09-10 00:06:10','0000-00-00 00:00:00'),(24,15,'/admin/home/rsync','','数据同步','<i class=\"layui-icon layui-icon-senior\"></i>',1,0,1,0,'','2018-09-10 23:42:03','0000-00-00 00:00:00');
+INSERT INTO `Menu` VALUES (1,0,'/admin/menu#','','后台管理','<i class=\"layui-icon layui-icon-set-fill\"></i>',1,1,1,0,'','2018-09-08 23:45:50','2018-09-09 00:18:33'),(2,1,'/admin/menu','','菜单管理','<i class=\"layui-icon layui-icon-find-fill\"></i>',1,1,1,0,'','2018-09-08 23:51:02','2018-09-09 00:15:51'),(3,2,'/admin/menu/add','','添加菜单','',1,0,1,0,'','2018-09-09 10:21:40','0000-00-00 00:00:00'),(4,2,'/admin/menu/edit','','修改菜单','',1,0,1,0,'','2018-09-09 10:22:07','0000-00-00 00:00:00'),(5,2,'/admin/menu/info','','菜单详情','',1,0,1,0,'','2018-09-09 10:23:05','0000-00-00 00:00:00'),(6,2,'/admin/menu/all','','所有菜单','',1,0,1,0,'','2018-09-09 10:23:33','0000-00-00 00:00:00'),(7,1,'/admin/role','','角色管理','<i class=\"layui-icon layui-icon-group\"></i>',1,1,1,0,'','2018-09-09 10:24:34','0000-00-00 00:00:00'),(8,7,'/admin/role/add','','添加角色','',1,0,1,0,'','2018-09-09 11:02:19','0000-00-00 00:00:00'),(9,7,'/admin/role/edit','','修改角色','',1,0,1,0,'','2018-09-09 11:04:55','0000-00-00 00:00:00'),(10,7,'/admin/role/info','','角色详情','',1,0,1,0,'','2018-09-09 11:14:33','0000-00-00 00:00:00'),(11,7,'/admin/role/all','','所有角色','',1,0,1,0,'','2018-09-09 11:14:58','0000-00-00 00:00:00'),(12,1,'/admin/master','','用户管理','<i class=\"layui-icon layui-icon-friends\"></i>',1,1,1,0,'','2018-09-09 17:16:04','0000-00-00 00:00:00'),(13,12,'/admin/master/add','','添加用户','',1,0,1,0,'','2018-09-09 17:16:35','2018-09-09 17:17:22'),(14,12,'/admin/master/edit','','修改用户','',1,0,1,0,'','2018-09-09 17:17:11','0000-00-00 00:00:00'),(15,0,'/admin/home#','','公共','<i class=\"layui-icon layui-icon-tips\"></i>',0,0,1,0,'','2018-09-09 23:48:57','0000-00-00 00:00:00'),(16,15,'/admin/home','','控制台','<i class=\"layui-icon layui-icon-console\"></i>',0,0,1,0,'','2018-09-09 23:49:32','0000-00-00 00:00:00'),(17,15,'/admin/home/nav','','导航菜单','',0,0,1,0,'','2018-09-09 23:50:36','0000-00-00 00:00:00'),(18,15,'/admin/home/upload','','文件上传','',0,0,1,0,'','2018-09-09 23:51:17','0000-00-00 00:00:00'),(19,0,'/admin/api#','','系统资料','<i class=\"layui-icon layui-icon-tabs\"></i>',1,1,1,0,'','2018-09-09 23:59:00','0000-00-00 00:00:00'),(20,19,'/admin/api','','接口文档','<i class=\"layui-icon layui-icon-link\"></i>',1,1,1,0,'','2018-09-10 00:04:29','0000-00-00 00:00:00'),(21,20,'/admin/api/add','','添加接口','',1,0,1,0,'','2018-09-10 00:05:02','0000-00-00 00:00:00'),(22,20,'/admin/api/edit','','修改接口','',1,0,1,0,'','2018-09-10 00:05:27','0000-00-00 00:00:00'),(23,20,'/admin/api/info','','接口信息','',1,0,1,0,'','2018-09-10 00:06:10','0000-00-00 00:00:00'),(24,15,'/admin/home/rsync','','数据同步','<i class=\"layui-icon layui-icon-senior\"></i>',1,0,1,0,'','2018-09-10 23:42:03','0000-00-00 00:00:00'),(25,20,'/admin/api/all','','所有接口','',1,0,1,0,'','2018-09-14 22:45:08','0000-00-00 00:00:00'),(26,19,'/admin/cate','','分类管理','<i class=\"layui-icon layui-icon-template-1\"></i>',1,1,1,0,'','2018-09-14 22:47:00','0000-00-00 00:00:00'),(27,26,'/admin/cate/add','','添加分类','',1,0,1,0,'','2018-09-14 22:47:28','0000-00-00 00:00:00'),(28,26,'/admin/cate/edit','','修改分类','',1,0,1,0,'','2018-09-14 22:47:43','0000-00-00 00:00:00'),(29,26,'/admin/cate/info','','分类信息','',1,0,1,0,'','2018-09-14 22:48:04','0000-00-00 00:00:00'),(30,26,'/admin/cate/update','','更新配置文件','',1,0,1,0,'','2018-09-14 22:48:20','0000-00-00 00:00:00'),(31,19,'/admin/source','','图片资源','<i class=\"layui-icon layui-icon-picture\"></i>',1,1,1,0,'','2018-09-14 22:48:55','0000-00-00 00:00:00'),(32,31,'/admin/source/add','','添加资源','',1,0,1,0,'','2018-09-14 22:49:14','0000-00-00 00:00:00'),(33,31,'/admin/source/edit','','修改资源','',1,0,1,0,'','2018-09-14 22:49:37','0000-00-00 00:00:00'),(34,31,'/admin/source/all','','所有资源','',1,0,1,0,'','2018-09-14 22:49:54','0000-00-00 00:00:00'),(35,31,'/admin/source/info','','资源信息','',1,0,1,0,'','2018-09-14 22:50:09','0000-00-00 00:00:00'),(36,19,'/admin/config','','快速配置','<i class=\"layui-icon layui-icon-util\"></i>',1,1,1,0,'','2018-09-14 22:50:50','0000-00-00 00:00:00'),(37,15,'/admin/master/modify','','修改账号','',0,0,1,0,'','2018-09-15 00:25:49','2018-09-15 00:27:04'),(38,15,'/admin/master/editPasswd','','修改密码','',0,0,1,0,'','2018-09-15 00:27:33','0000-00-00 00:00:00'),(39,36,'/admin/config/file','','配置文件','',1,0,1,0,'','2018-09-15 15:21:01','0000-00-00 00:00:00'),(40,36,'/admin/config/edit','','编辑配置','',1,0,1,0,'','2018-09-15 15:22:44','0000-00-00 00:00:00'),(41,36,'/admin/config/data','','查看数据','',1,0,1,0,'','2018-09-15 15:23:06','0000-00-00 00:00:00'),(42,36,'/admin/config/download','','下载配置','',1,0,1,0,'','2018-09-15 15:23:39','0000-00-00 00:00:00'),(43,36,'/admin/config/del','','删除配置','',1,0,1,0,'','2018-09-15 15:24:15','0000-00-00 00:00:00'),(44,36,'/admin/config/update','','更新配置','',1,0,1,0,'','2018-09-15 15:24:52','0000-00-00 00:00:00'),(45,36,'/admin/config/remove','','删除文件','',1,0,1,0,'','2018-09-15 15:26:13','0000-00-00 00:00:00'),(46,19,'/admin/file','','文件管理','<i class=\"layui-icon layui-icon-upload\"></i>',1,1,1,0,'','2018-09-16 19:50:50','2018-09-16 21:50:21'),(47,46,'/admin/file/del','','删除文件','',1,0,1,0,'','2018-09-16 19:51:17','0000-00-00 00:00:00'),(48,19,'/admin/log','','日志管理','<i class=\"layui-icon layui-icon-survey\"></i>',1,1,1,0,'','2018-09-16 19:52:29','0000-00-00 00:00:00'),(49,48,'/admin/log/info','','日志详情','',1,0,1,0,'','2018-09-16 19:53:32','0000-00-00 00:00:00'),(50,48,'/admin/log/del','','删除日志','',1,0,1,0,'','2018-09-16 19:54:13','0000-00-00 00:00:00'),(51,48,'/admin/log/download','','下载日志','',1,0,1,0,'','2018-09-16 20:47:00','0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `Menu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -196,10 +225,14 @@ LOCK TABLES `Role` WRITE;
 INSERT INTO `Role` VALUES (1,0,'开发者',1,'','','2018-09-09 11:42:15','2018-09-09 15:42:33'),(2,0,'超级管理员',1,'','','2018-09-09 11:43:02','2018-09-09 15:29:33');
 /*!40000 ALTER TABLE `Role` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+--
+-- Table structure for table `Source`
+--
 
 DROP TABLE IF EXISTS `Source`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8 ;
 CREATE TABLE `Source` (
   `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL COMMENT '名称',
@@ -210,6 +243,17 @@ CREATE TABLE `Source` (
   `Updated` datetime DEFAULT NULL,
   PRIMARY KEY (`Id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='图片资源';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Source`
+--
+
+LOCK TABLES `Source` WRITE;
+/*!40000 ALTER TABLE `Source` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Source` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -219,4 +263,4 @@ CREATE TABLE `Source` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-11  0:03:23
+-- Dump completed on 2018-09-16 21:58:23
