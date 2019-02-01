cf4eb907e8905399483de069de0822e824660c75
yunzhong_php <php@yunzhong.com>
2019-02-01 17:59:35 +0800

    生成插入或更新数据（包含增量更新）的sql

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -69,38 +69,89 @@ if(!function_exists('getChildsAndLast')){
     }
 }
 
+if(!function_exists('insertOrUpdateSql')){
+
+    /** 生成插入或更新数据的sql
+     * @param string $table
+     * @param array $data
+     * @param string|array $updateFields 覆盖更新的字段
+     * @param string|array $incrFields   增量更新的字段
+     * @return bool|string
+     */
+    function insertOrUpdateSql($table, array $data, $updateFields = array(), $incrFields = array())
+    {
+        if(0 == count($data)){
+            return false;
+        }
+        $sql = "insert into `$table` ";
+        # 数据字段
+        foreach($data as $key => $val){
+            $val = addslashes($val);
+            $sql .= " set `$key` = '$val',";
+        }
+        $sql = rtrim($sql,', ');
+        # 更新字段
+        if(empty($updateFields) && empty($incrFields)){
+            return $sql;
+        }
+        $sql .= ' on duplicate key update ';
+        # 覆盖更新的字段
+        if(!empty($updateFields)){
+            if(!is_array($updateFields)){
+                $updateFields = array($updateFields);
+            }
+            foreach($updateFields as $field){
+                $sql .= " `$field` = values(`$field`), ";
+            }
+            $sql = rtrim($sql,', ');
+        }
+        # 增量更新字段
+        if(!empty($incrFields)){
+            if(!is_array($incrFields)){
+                $incrFields = array($incrFields);
+            }
+            foreach($incrFields as $field){
+                $sql .= " `{$field}` = `{$field}` + values(`{$field}`), ";
+            }
+            $sql = rtrim($sql,', ');
+        }
+
+        return $sql;
+    }
+}
+
 if(!function_exists('batchInsertOrUpdateSql')){
     /** 批量 更新或插入数据的sql
      * @param string $table         数据表名
      * @param array $inserts        数据字段
      * @param array $values         原始数据
-     * @param array|string $updates 更新字段
+     * @param array|string $updates 覆盖更新字段
+     * @param array|string $incrs   增量更新字段
      * @return bool|array          返回false(条件不符)，返回array(sql语句)
      */
-    function batchInsertOrUpdateSql($table='', $values=array(), $inserts=array(), $updates=array()){
+    function batchInsertOrUpdateSql($table='', $values=array(), $inserts=array(), $updates=array(), $incrs=array()){
         if(empty($table) || empty($inserts) || empty($values)){
             return false;
         }
         if(!empty($updates)){
-            // 数据字段必须包含更新字段
-            if(is_string($updates)){
-                if(!in_array($updates,$inserts)){
-                    return false;
-                }
-            }else{
-                $commons= array_intersect($inserts,$updates);
-                sort($commons);
-                sort($updates);
-                if($commons != $updates){
-                    return false;
-                }
+            // 数据字段必须包含覆盖更新字段
+            $checked = checkLieInList($updates, $inserts);
+            if(false == $checked){
+                return false;
+            }
+        }
+        if(!empty($incrs)){
+            // 数据字段必须包含增量更新字段
+            $checked = checkLieInList($incrs, $inserts);
+            if(false == $checked){
+                return false;
             }
         }
 
         //数据字段
         $sql_inserts=array();
         foreach ($inserts as $insert){
-            $sql_inserts[]='`'.$insert.'`';
+            $sql_inserts[]=" `$insert` ";
         }
         $sql_inserts=implode(',',$sql_inserts);
         //数据分页
@@ -114,32 +165,47 @@ if(!function_exists('batchInsertOrUpdateSql')){
             foreach ($inserts as $insert){
                 $temp=isset($values[$i][$insert]) && !is_null($values[$i][$insert])?(string)$values[$i][$insert]:null;
                 $temp = addslashes($temp);
-                $temp_values[]="'".$temp."'";
+                $temp_values[]=" '$temp' ";
             }
             $temp_values=implode(',',$temp_values);
-            $page_values[$p][]='('.$temp_values.')';
+            $page_values[$p][]=" ($temp_values) ";
         }
+        $updateSql = '';
+        if(!empty($updates) || !empty($incrs)){
+            $updateSql = ' on duplicate key update ';
+        }
+        // 覆盖更新的字段
         if(!empty($updates)){
-            //更新字段
             if(is_string($updates)){
-                $sql_updates= ' `'.$updates.'` = values(`'.$updates.'`) ';
+                $sql_updates = " `$updates` = values(`$updates`) ";
             }else{
                 $sql_updates=array();
                 foreach ($updates as $update){
-                    $sql_updates[]= ' `'.$update.'` = values(`'.$update.'`) ';
+                    $sql_updates[] = " `$updates` = values(`$updates`) ";
                 }
                 $sql_updates=implode(',',$sql_updates);
             }
+            $updateSql .= $sql_updates;
+        }
+        // 增量更新字段
+        if(!empty($incrs)){
+            if(is_string($incrs)){
+                $sql_incrs= " `{$incrs}` = `{$incrs}` + values(`{$incrs}`) ";
+            }else{
+                $sql_incrs=array();
+                foreach ($incrs as $incr){
+                    $sql_incrs[]= " `{$incr}` = `{$incr}` + values(`{$incr}`) ";
+                }
+                $sql_incrs = implode(',',$sql_incrs);
+            }
+            $updateSql .= $sql_incrs;
         }
 
         // 生成sql
         $sqls=array();
         for($i=0;$i<$p;$i++){
             $sql_values=implode(',',$page_values[$i+1]);
-            $sqls[$i]='insert into `'.$table.'` ('.$sql_inserts.') values '.$sql_values;
-            if(!empty($updates)){
-                $sqls[$i] .=' on duplicate key update '.$sql_updates;
-            }
+            $sqls[$i]="insert into `$table` ($sql_inserts) values $sql_values $updateSql";
         }
         return $sqls;
     }
@@ -159,42 +225,27 @@ if(!function_exists('batchUpdateSql')){
             return false;
         }
         // 数据字段必须包含更新字段
-        if(is_string($updates)){
-            if(!in_array($updates,$inserts)){
-                return false;
-            }
-        }else{
-            $commons= array_intersect($inserts,$updates);
-            sort($commons);
-            sort($updates);
-            if($commons != $updates){
-                return false;
-            }
+        $checked = checkLieInList($updates, $inserts);
+        if(false == $checked){
+            return false;
         }
         // 数据字段必须包含条件字段
-        if(is_string($wheres)){
-            if(!in_array($wheres,$inserts)){
-                return false;
-            }
-        }else{
-            $commons= array_intersect($inserts,$wheres);
-            sort($commons);
-            sort($wheres);
-            if($commons != $wheres){
-                return false;
-            }
+        $checked = checkLieInList($wheres, $inserts);
+        if(false == $checked){
+            return false;
         }
+
         //数据字段
         $sql_inserts=array();
         foreach ($inserts as $insert){
-            $sql_inserts[]='`'.$insert.'`';
+            $sql_inserts[]=" $insert ";
         }
         $sql_inserts=implode(',',$sql_inserts);
         /* ++++++++++ 创建虚拟表 ++++++++++ */
         //创建虚拟表 表名
-        $temp_table='`'.$table.'_temp`';
+        $temp_table=" `{$table}_temp` ";
         //创建虚拟表 sql
-        $sqls[]='create temporary table '.$temp_table.' as ( select '.$sql_inserts.' from `'.$table.'` where 1<>1 )';
+        $sqls[] = " create temporary table $temp_table as ( select $sql_inserts from `$table` where 1<>1 ) ";
         /* ++++++++++ 添加数据 ++++++++++ */
         //数据分页
         $num=100;
@@ -207,40 +258,66 @@ if(!function_exists('batchUpdateSql')){
             foreach ($inserts as $insert){
                 $temp=isset($values[$i][$insert]) && !is_null($values[$i][$insert])?(string)$values[$i][$insert]:null;
                 $temp = addslashes($temp);
-                $temp_values[]="'".$temp."'";
+                $temp_values[]=" '$temp' ";
             }
             $temp_values=implode(',',$temp_values);
-            $page_values[$p][]='('.$temp_values.')';
+            $page_values[$p][]=" ($temp_values) ";
         }
         //插入数据 sql
         for($i=0;$i<$p;$i++){
             $sql_values=implode(',',$page_values[$i+1]);
-            $sqls[]='insert into '.$temp_table.' ('.$sql_inserts.') values '.$sql_values;
+            $sqls[]=" insert into $temp_table ($sql_inserts) values $sql_values ";
         }
         /* ++++++++++ 批量更新 ++++++++++ */
         //更新字段
         if(is_string($updates)){
-            $sql_updates= '`'.$table.'`.`'.$updates.'`='.$temp_table.'.`'.$updates.'`';
+            $sql_updates= " `$table`.`$updates` = `$temp_table`.`$updates` ";
         }else{
             $sql_updates=array();
             foreach ($updates as $update){
-                $sql_updates[]= '`'.$table.'`.`'.$update.'`='.$temp_table.'.`'.$update.'`';
+                $sql_updates[] = " `$table`.`$updates` = `$temp_table`.`$updates` ";
             }
             $sql_updates=implode(',',$sql_updates);
         }
         //条件字段
         if(is_string($wheres)){
-            $sql_wheres= '`'.$table.'`.`'.$wheres.'`='.$temp_table.'.`'.$wheres.'`';
+            $sql_wheres= " `$table`.`$wheres` = `$temp_table`.`$wheres` ";
         }else{
             $sql_wheres=array();
             foreach ($wheres as $where){
-                $sql_wheres[]= '`'.$table.'`.`'.$where.'`='.$temp_table.'.`'.$where.'`';
+                $sql_wheres[] = " `$table`.`$wheres` = `$temp_table`.`$wheres` ";
             }
             $sql_wheres=implode(' and ',$sql_wheres);
         }
         //更新数据 sql
-        $sqls[]='update `'.$table.'`,'.$temp_table.' set '.$sql_updates.' where '.$sql_wheres;
+        $sqls[]="update `$table` , $temp_table set $sql_updates where $sql_wheres ";
         return $sqls;
+    }
+}
+
+if(!function_exists('checkLieInList')){
+
+    /** 检查元素是否包含于列表中
+     * @param mixed $check
+     * @param array $list
+     * @return bool
+     */
+    function checkLieInList($check, array $list)
+    {
+        if(is_string($check)){
+            if(!in_array($check,$list)){
+                return false;
+            }
+        }else{
+            $commons= array_intersect($check,$list);
+            sort($commons);
+            sort($check);
+            if($commons != $check){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
 
